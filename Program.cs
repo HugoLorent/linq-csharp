1d5307622d0cc126352c51d6410e770a85675e6e
Rémy MESTRE <remy.mestre@campus-igs-toulouse.fr>
2022-01-11 14:44:54 +0100

    Indent JSON file

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace linq_csharp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Recherche un trajet :");
+            //string recherche = Console.ReadLine();
+            var myJsonTravels = JObject.Parse(File.ReadAllText($@"{Directory.GetCurrentDirectory()}/JSON/tarifs-tgv-par-od.json"));
+
+            Console.WriteLine(myJsonTravels);
+
+            var queryTravels = from travel in myJsonTravels["AllTravels"]["fields"]
+                            let line = travel["od"].ToString()
+                            //where line.Contains(recherche, StringComparison.InvariantCultureIgnoreCase)
+                            select line;
+            foreach (var travel in queryTravels)
+            {
+                Console.WriteLine(travel);
+            }
         }
     }
 }
