88354ad76da4a849fd38a7b833e892e51b77083e
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-02-02 13:48:57 +0100

    wip
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using linq_csharp.JSON;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -11,12 +12,61 @@ namespace linq_csharp
 
         static void Main(string[] args)
         {
-            Xml xml = new Xml();
-            xml.main();
-            TrainJSON json = new TrainJSON();
-            // json.proposerDépart();
+            bool restart = true;
+            while (restart)
+            {
 
+                List<string> sources = new List<string>();
+                sources.Add("json");
+                sources.Add("xml");
+                Console.WriteLine("Vos deux sources sont : json et xml");
+                Console.WriteLine("Tapez pour continuer");
+                Console.ReadLine();
+                Console.WriteLine("Veuillez saisir votre source d'informations");
+                string sourceVoulue = Console.ReadLine();
+                bool isSourceCorrect = false;
 
+                if (sourceVoulue == "json")
+                {
+                    TrainJSON json = new TrainJSON();
+                    json.proposerDépart();
+                }
+                else if (sourceVoulue == "xml")
+                {
+                    Xml xml = new Xml();
+                    xml.main();
+                }
+                else
+                {
+                    foreach (var source in sources)
+                    {
+                        if (source == sourceVoulue)
+                        {
+                            isSourceCorrect = true;
+                        }
+                    }
+                    while (!isSourceCorrect)
+                    {
+                        Console.WriteLine("Votre source n'est pas correct, veulliez recommencer");
+                        sourceVoulue = Console.ReadLine();
+
+                        foreach (var source in sources)
+                        {
+                            if (source == sourceVoulue)
+                            {
+                                isSourceCorrect = true;
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine("Voulez-vous refaire une recherche ? Répondez par o ou n");
+                string choix = Console.ReadLine();
+                if (choix == "n")
+                {
+                    restart = false;
+                }
+            }
         }
     }
 }
