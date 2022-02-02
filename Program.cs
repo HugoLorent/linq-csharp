362001ec628dc8251f68e10eaae734bb965f17ed
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-02-02 09:03:03 +0100

    wip
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -10,17 +10,8 @@ namespace linq_csharp
 
         static void Main(string[] args)
         {
-            //    Console.WriteLine("Recherche un trajet :");
-            //    string recherche = Console.ReadLine();
-            var myJsonTravels = JArray.Parse(File.ReadAllText($@"{Directory.GetCurrentDirectory()}/JSON/tarifs-tgv-par-od.json"));
-            var queryTravels = from travel in myJsonTravels
-                            let line = travel["fields"]["od"]
-                            //where line.Contains(recherche, StringComparison.InvariantCultureIgnoreCase)
-                            select line;
-            foreach (var travel in queryTravels)
-            {
-                Console.WriteLine(travel);
-            }
+            Xml xml = new Xml();
+            xml.proposerDepart();
         }
     }
 }
---
+++ XML/Xml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace linq_csharp
+{
+    class Xml
+    {
+        public void proposerDepart()
+        {
+            Console.WriteLine("Ou voulez-vous aller ?");
+            var rechercheArrivee = Console.ReadLine();
+            var xmlTravels = XElement.Load($@"{Directory.GetCurrentDirectory()}/XML/tarifs-tgv-par-od.xml");
+
+            var travelRequest = from travel in xmlTravels.Descendants("fields")
+                                let arrivee = travel.Element("od").Value
+                                where arrivee.Contains(rechercheArrivee, StringComparison.InvariantCultureIgnoreCase)
+                                select arrivee;
+
+            foreach (var travel in travelRequest)
+            {
+                Console.WriteLine(travel);
+            }
+        }
+    }
+}
